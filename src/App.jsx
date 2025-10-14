b4c5f015bc17c9e78ff239d4873acf4a2dcdb967
Brian <brtl765@gmail.com>
2025-10-14 16:08:57 -0400

    change title part 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
-// import { useState, useEffect } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from 'react'
+import './App.css'
 import Preloader from "../src/components/Pre";
 import Navbar from "./components/Navbar/Navbar.jsx";
 import Home from "./components/Home/Home";
